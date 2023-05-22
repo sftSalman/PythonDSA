574f2e12606f52b31bf43e54c3d72d44d652bba4
salmanFarshi <salmanfarshinsu@gmail.com>
2023-05-22 22:53:02 +0600

    explanation added

--- move_all_zerp.py
+++ move_all_zerp.py
@@ -7,3 +7,24 @@ def move_zero(arr):
     return arr
 
 print(move_zero(arr))
+
+'''
+arr = [1, 2, 0, 4, 3, 0, 5, 0]
+result = []
+
+for num in arr:
+    if num != 0:
+        result.append(num)
+
+print(result)
+
+
+
+arr = [1,2,3,0,2,3,0,2]
+new_arr = [22]
+zerr_arr = [1]*3
+print(zerr_arr)
+arr.extend(new_arr)
+print(arr)
+
+'''
