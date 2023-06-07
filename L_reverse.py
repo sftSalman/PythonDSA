ac56cb18f832e7803d4d2ea7a81e264ad82f802e
salmanFarshi <salmanfarshinsu@gmail.com>
2023-06-07 22:40:35 +0600

    iterative way

--- L_reverse.py
+++ L_reverse.py
@@ -7,14 +7,6 @@ class LinkedList:
     def __init__(self, head=None):
         self.head = head
 
-    def lenght(self):
-        count = 0
-        currentNode = self.head
-        while currentNode is not None:
-            count = count +1
-            currentNode = currentNode.nextNode
-        return count
-
     def insert(self, value):
         node = linkedListNode(value)
         node.nextNode = self.head
@@ -27,7 +19,7 @@ class LinkedList:
             currentNode = currentNode.nextNode
         print('None')
 
-    def search(self,value):
+    def search(self, value):
         currentNode = self.head
         while currentNode is not None:
             if currentNode.value == value:
@@ -35,18 +27,39 @@ class LinkedList:
             currentNode = currentNode.nextNode
         return False
 
+    def get_length(self):
+        count = 0
+        currentNode = self.head
+        while currentNode is not None:
+            count += 1
+            currentNode = currentNode.nextNode
+        return count
+
+    def reverse(self):
+        prevNode = None
+        currentNode = self.head
+        while currentNode is not None:
+            nextNode = currentNode.nextNode
+            currentNode.nextNode = prevNode
+            prevNode = currentNode
+            currentNode = nextNode
+        self.head = prevNode
+
 # Create a linked list object
 l1 = LinkedList()
 
-# Insert values at the beginning and print the list
+# Insert values at the beginning
 l1.insert(6)
-l1.printList()
-
 l1.insert(7)
-l1.printList()
-
 l1.insert(8)
 
+# Print the original list
+print("Original list:")
 l1.printList()
-print(l1.search(10))
-print(l1.lenght())
+
+# Reverse the list
+l1.reverse()
+
+# Print the reversed list
+print("Reversed list:")
+l1.printList()
