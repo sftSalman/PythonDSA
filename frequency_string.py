227047ebbd1628eb31c4ae0abeed3c36766913c4
salmanFarshi <salmanfarshinsu@gmail.com>
2023-06-27 23:00:53 +0600

    details added for better understanding

--- frequency_string.py
+++ frequency_string.py
@@ -1,3 +1,19 @@
+st = 'aabccccddddddd'
+
+def freq(st):
+    result = ''
+    count = 1
+    for i in range(len(st)-1):
+        if st[i] == st[i+1]:
+            count += 1
+        else:
+            result += st[i] + str(count)
+            count = 1
+    result += st[-1] + str(count)  # Add the last character and its count
+    return result
+
+print(freq(st))
+
 st = 'aabccccdd'
 print(len(st))
 
