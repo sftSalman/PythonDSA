65c3d78e5a55605df16c6030d1a12c325d7278d0
salmanFarshi <salmanfarshinsu@gmail.com>
2023-07-14 00:22:45 +0600

    --------

--- L1_swapNode.py
+++ L1_swapNode.py
@@ -41,8 +41,37 @@ class LinkedList:
         prev1 = None
         cur1 = self.head
         while cur1 and cur1.value != k1:
+
+
             prev1 = cur1
             cur1=cur1.nextNode
+
+
+
+        prev2 = None
+        cur2 = self.head
+        while cur2 and cur2.value != k2:
+
+
+            prev2 = cur2
+            cur2=cur2.nextNode
+
+
+        if not cur1 or not cur2:
+            return
+        if prev1:
+            prev1.nextNode = cur2
+        else:
+            self.head = cur2
+        if prev2:
+            prev2.nextNode = cur1
+        else:
+            self.head = cur1
+        cur1.nextNode ,cur2.nextNode = cur2.nextNode,cur1.nextNode
+
+
+
+
 
 
     # def reverse(self):
@@ -66,4 +95,6 @@ l1.insert(8)
 l1.printList()
 print(l1.search(10))
 l1.rev_it()
+l1.printList()
+l1.swap(7,8)
 l1.printList()
