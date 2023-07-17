85f551c90cc975d49303d58594620b27ae676916
salmanFarshi <salmanfarshinsu@gmail.com>
2023-07-17 21:58:30 +0600

    need to revised

--- BST.py
+++ BST.py
@@ -8,6 +8,19 @@ class Node(object):
 class BT(object):
     def __init__(self,root):
         self.root = Node(root)
+    def print_tree(self, traversal_type):
+        if traversal_type == "preorder":
+            return self.preorder_print(tree.root, "")
+        # elif traversal_type == "inorder":
+        #     return self.inorder_print(tree.root, "")
+        # elif traversal_type == "postorder":
+        #     return self.postorder_print(tree.root, "")
+        # elif traversal_type == "levelorder":
+        #     return self.levelorder_print(tree.root)
+
+        else:
+            print("Traversal type " + str(traversal_type) + " is not supported.")
+            return False
 
     def preorder_print(self,start , traversal):
         # root - left-right
@@ -15,6 +28,21 @@ class BT(object):
             traversal += (str(start.value)+ " - ")
             traversal = self.preorder_print(start.left,traversal)
             traversal = self.preorder_print(start.right,traversal)
+        return traversal
+    def inorder_print(self, start, traversal):
+        """Left->Root->Right"""
+        if start:
+            traversal = self.inorder_print(start.left, traversal)
+            traversal += (str(start.value) + "-")
+            traversal = self.inorder_print(start.right, traversal)
+        return traversal
+
+    def postorder_print(self, start, traversal):
+        """Left->Right->Root"""
+        if start:
+            traversal = self.inorder_print(start.left, traversal)
+            traversal = self.inorder_print(start.right, traversal)
+            traversal += (str(start.value) + "-")
         return traversal
 
 
@@ -26,6 +54,7 @@ tree.root.left = Node(2)
 tree.root.right = Node(3)
 tree.root.left.left = Node(4)
 tree.root.left.right = Node(5)
+print(tree.print_tree('preorder'))
 
 
 
---
+++ Recursion_basic.py
@@ -0,0 +1,9 @@
+def findLenght(st):
+    count = 0
+    for i in st:
+        count = count + 1
+    return count
+
+print(findLenght('salman'))
+
+def recur_lenght(st,idx=0):
