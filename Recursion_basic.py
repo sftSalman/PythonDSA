912262bdf8f3e8acbf9ff40785dfc058307c809b
salmanFarshi <salmanfarshinsu@gmail.com>
2023-07-19 14:36:06 +0600

    youtubr https://www.youtube.com/watch?v=IJDJ0kBx2LM&ab_channel=freeCodeCamp.org

--- Recursion_basic.py
+++ Recursion_basic.py
@@ -1,3 +1,6 @@
+
+#https://www.youtube.com/watch?v=IJDJ0kBx2LM&ab_channel=freeCodeCamp.org
+
 def findLenght(st):
     count = 0
     for i in st:
--- s2_reverse_string_iterative_recursion.py
+++ s2_reverse_string_iterative_recursion.py
@@ -10,20 +10,20 @@ def rev(st):
     if len(st) <= 1:
         return st
     else:
-        return rev(st[1:]) + st[0]
-
-"d" + rev("lroW" + " " + "," + "o" + "l" + "l" + "e" + "H")
-"dl" + rev("roW" + " " + "," + "o" + "l" + "l" + "e" + "H")
-"dlr" + rev("oW" + " " + "," + "o" + "l" + "l" + "e" + "H")
-"dlro" + rev("W" + " " + "," + "o" + "l" + "l" + "e" + "H")
-"dlroW" + rev(" " + "," + "o" + "l" + "l" + "e" + "H")
-"dlroW " + rev("," + "o" + "l" + "l" + "e" + "H")
-"dlroW ," + rev("o" + "l" + "l" + "e" + "H")
-"dlroW ,o" + rev("l" + "l" + "e" + "H")
-"dlroW ,ol" + rev("l" + "e" + "H")
-"dlroW ,oll" + rev("e" + "H")
-"dlroW ,olle" + rev("H")
-"dlroW ,olleH"
+        return  rev(st[1:]) +st[0]
+#
+# "d" + rev("lroW" + " " + "," + "o" + "l" + "l" + "e" + "H")
+# "dl" + rev("roW" + " " + "," + "o" + "l" + "l" + "e" + "H")
+# "dlr" + rev("oW" + " " + "," + "o" + "l" + "l" + "e" + "H")
+# "dlro" + rev("W" + " " + "," + "o" + "l" + "l" + "e" + "H")
+# "dlroW" + rev(" " + "," + "o" + "l" + "l" + "e" + "H")
+# "dlroW " + rev("," + "o" + "l" + "l" + "e" + "H")
+# "dlroW ," + rev("o" + "l" + "l" + "e" + "H")
+# "dlroW ,o" + rev("l" + "l" + "e" + "H")
+# "dlroW ,ol" + rev("l" + "e" + "H")
+# "dlroW ,oll" + rev("e" + "H")
+# "dlroW ,olle" + rev("H")
+# "dlroW ,olleH"
 
 
 # Example usage
