e9c4fe7c8cf9b48d390168f7774840264392e82c
salmanFarshi <salmanfarshinsu@gmail.com>
2023-07-24 00:08:16 +0600

    --arr-subarr

---
+++ --arr_generate_subarray.py
@@ -0,0 +1,14 @@
+def generate_subarrays(arr):
+    subarrays = []
+    n = len(arr)
+
+    for start in range(n):
+        for end in range(start, n):
+            subarray = arr[start:end + 1]
+            subarrays.append(subarray)
+
+    return subarrays
+
+arr = [1, 2, 3]
+all_subarrays = generate_subarrays(arr)
+print(*all_subarrays)
