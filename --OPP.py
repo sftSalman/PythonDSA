0e7565477c66ae2e0a145956b90e28066c8a3f36
salmanFarshi <salmanfarshinsu@gmail.com>
2023-07-24 16:21:31 +0600

    must be revised

--- --OPP.py
+++ --OPP.py
@@ -1,6 +1,7 @@
-# https://www.freecodecamp.org/news/object-oriented-programming-in-python/
+from abc import ABC, abstractmethod
 
-class Book:
+
+class Book(ABC):
     def __init__(self, title, quantity, author, price):
         self.title = title
         self.quantity = quantity
@@ -16,17 +17,33 @@ class Book:
             return self.__price * (1-self.__discount)
         return self.__price
 
+    @abstractmethod
     def __repr__(self):
         return f"Book: {self.title}, Quantity: {self.quantity}, Author: {self.author}, Price: {self.get_price()}"
 
 
+class Novel(Book):
+    def __init__(self, title, quantity, author, price, pages):
+        super().__init__(title, quantity, author, price)
+        self.pages = pages
 
-single_book = Book('Two States', 1, 'Chetan Bhagat', 200)
+    def __repr__(self) :
+        return f"Book: {self.title}, Quantity: {self.quantity}, Author: {self.author}, Price: {self.get_price ()}"
 
-bulk_books = Book('Two States', 25, 'Chetan Bhagat', 200)
-bulk_books.set_discount(0.20)
 
-print(single_book.get_price())
-print(bulk_books.get_price())
-print(single_book)
-print(bulk_books)
+class Academic(Book):
+    def __init__(self, title, quantity, author, price, branch):
+        super().__init__(title, quantity, author, price)
+        self.branch = branch
+
+    def __repr__(self):
+        return f"Book: {self.title}, Branch: {self.branch}, Quantity: {self.quantity}, Author: {self.author}, Price: {self.get_price()}"
+
+
+novel1 = Novel('Two States', 20, 'Chetan Bhagat', 200, 187)
+novel1.set_discount(0.20)
+
+academic1 = Academic('Python Foundations', 12, 'PSF', 655, 'IT')
+
+print(novel1)
+print(academic1)
